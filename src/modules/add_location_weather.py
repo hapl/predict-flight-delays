c0363863e6413e2da882a3972c21f6ffd46a6bc3
Hugo <hugo.perdomo@gmail.com>
2023-03-22 15:54:47 -0400

    Update function add_location and add_weather
    Given a dataset, the functions will do the following:
    --> add_location: It will add the latitude (lat) and longitude(lon) based on airports data located in the folder data/processed/Airports_lat_lon.csv
    --> add_weather: it will add the weather components to the dataset based on the historic information (2018,2019 and January 2020)

--- src/modules/add_location_weather.py
+++ src/modules/add_location_weather.py
@@ -6,5 +6,9 @@ def add_location(df,airports_ids):
     result = pd.merge(df,airports_ids, left_on = 'origin_airport_id', right_on='airport_id' )
     return result 
 def add_weather(df,weather_airports):
-    results = pd.merge(df,weather_airports, left_on =['Key','date'], right_on=['Key','fl_date'])
+    df['fl_date'] = '01'+'-'+ df['day'].astype(str) +'-'+'2020'
+    df['fl_date'] = df['fl_date'].astype('datetime64')
+    weather_airports['fl_date'] = weather_airports['fl_date'].astype('datetime64')
+    results = pd.merge(df,weather_airports, on =['Key','fl_date'], how ='left')
+    results.drop_duplicates(inplace=True)
     return results  
